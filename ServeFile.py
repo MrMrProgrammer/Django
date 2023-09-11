4a8a077d6c58301f06ff031174b4c6590bf38c5b
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-09-11 21:57:35 +0430

    add serve file

---
+++ ServeFile.py
@@ -0,0 +1,37 @@
+# ===========================================================
+# Settings.py
+
+MEDIA_URL = '/photos/'
+
+
+# ===========================================================
+# urls.py
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# ===========================================================
+# views.py
+
+from django.views.generic import CreateView, ListView
+
+class -class name- (ListView):
+    template_name = '-template address-'
+    model = -model name-
+    context_object_name = 'files'
+
+
+# ===========================================================
+# template
+
+{% for file in files %}
+
+    <img src="{{ file.image.url }}" />
+
+{% endfor %}
+
+
+# ===========================================================
