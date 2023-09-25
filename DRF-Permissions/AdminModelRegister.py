0a6e85c760960cbe7bdb49d5d0fdd90d5e84413b
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-09-25 20:55:28 +0330

    regester models in admin

---
+++ DRF-Permissions/AdminModelRegister.py
@@ -0,0 +1,17 @@
+# ===========================================================
+# admin.py
+
+
+from django.contrib import admin
+from . import models
+
+class showDay(admin.ModelAdmin):
+    list_display = ["__str__"]
+    list_filter = ["fields"]
+    list_editable = ["fields"]
+    list_filter = ["fields"]
+
+admin.site.register(models.Days, showDay)
+
+
+# ===========================================================
