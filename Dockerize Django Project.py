d04f6ffe6a24cf0240c26c1ef1834733e44f8f95
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-10-25 11:03:59 +0330

    Dockerize Django Project

--- Dockerize Django Project.py
+++ Dockerize Django Project.py
@@ -1,4 +1,16 @@
-docker build -t mydjangoimage .
+# ===========================================================
+# Terminal
 
-docker run -v /home/user/Desktop/SiteIntroduction:/app -p 8000:8000 mydjangoimage
+# 1. Create an image :
 
+    # docker build -t image-name .
+
+
+# 2. Create a container :
+
+    # docker run -d -p 8000:8000 -v /path/to/project/directory:/app image-name python manage.py runserver 0.0.0.0:8000
+
+    # (Give the address of your project instead of /path/to/project/directory)
+
+
+# ===========================================================
