88ddaa6fc06ac24a4aecd67c510747fa72e7be49
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-10-25 11:07:43 +0330

    ALLOWED HOSTS settings

--- Allowed Hosts Settings.py
+++ Allowed Hosts Settings.py
@@ -1,19 +1,25 @@
 # ===========================================================
 # Settings.py
 
+
 DEBUG = False
 ALLOWED_HOSTS = ['your IP address', '127.0.0.1', 'localhost']
 
+
 # ===========================================================
 # Turn off the firewall
+
 
 # In Ubuntu, you can turn off the firewall with the following command
 
 # ' sudo ufw disable '
 
+
 # ===========================================================
 # Terminal
 
+
 # python manage.py runserver [your IP address]:8000
+
 
 # ===========================================================
