be44376a531c1f5200994a7d44b1dd8d7c7d4cfc
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-10-25 11:05:30 +0330

    Admin Model Register

--- Admin Model Register.py
+++ Admin Model Register.py
@@ -3,13 +3,12 @@
 
 
 from django.contrib import admin
-from .DRF-Permissions import models
+from .app-name import models
 
 class showDay(admin.ModelAdmin):
     list_display = ["__str__"]
     list_filter = ["fields"]
     list_editable = ["fields"]
-    list_filter = ["fields"]
 
 admin.site.register(models.Days, showDay)
 
