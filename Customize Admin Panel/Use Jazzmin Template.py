e8f2fb69ad25f5c95a5ac97648904ba06882e609
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-10-29 10:50:20 +0330

    how to use jazzmin template

--- Customize Admin Panel/Use Jazzmin Template.py
+++ Customize Admin Panel/Use Jazzmin Template.py
@@ -1,2 +1,25 @@
 # ===========================================================
-# 
+# terminal
+
+
+# pip install -U django-jazzmin
+
+# page in pypi : https://pypi.org/project/django-jazzmin/
+# pip install django-jazzmin
+
+
+# ===========================================================
+# settings.py
+
+
+INSTALLED_APPS = [
+    'jazzmin',
+
+    'django.contrib.admin',
+    [...]
+]
+
+# Attention : This library must be added before 'django.contrib.admin'
+
+
+# ===========================================================
