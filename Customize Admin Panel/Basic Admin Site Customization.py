92023a53282b3f24f3f95901f6128465d1b2d7ff
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-10-29 10:53:59 +0330

    add Basic Admin Site Customization file

---
+++ Customize Admin Panel/Basic Admin Site Customization.py
@@ -0,0 +1,10 @@
+# ===========================================================
+# project-name/urls.py
+
+
+admin.site.site_title = "TicketsPlus site admin (DEV)"
+admin.site.site_header = "TicketsPlus administration"
+admin.site.index_title = "Site administration"
+
+
+# ===========================================================
