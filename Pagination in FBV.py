df5725badedd15c16bb8692b7b2438fcbe920b6d
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-11-14 16:39:37 +0330

    Creating pagination in FBV

--- Pagination in FBV.py
+++ Pagination in FBV.py
@@ -25,3 +25,36 @@ def function_name(request):
     }
 
     return render(request, '', context)
+
+
+# ===========================================================
+# template.html
+
+
+<nav aria-label="Page navigation example">
+    <ul class="pagination page-number">
+
+        {% if objects.has_previous %}
+            <li class="page-item">
+                <a class="page-link" href="?page={{ objects.previous_page_number }}" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+
+        {% endif %}
+
+        {% for pageNumber in objects.paginator.page_range %}
+            <li class="page-item">
+                <a class="page-link {% if objects.number == pageNumber %}page_active{% endif %}" href="?page={{ pageNumber }}" {% if objects.number == pageNumber %}style="color: white; background-color: #0a660a" {% endif %}>{{ pageNumber }} </a>
+            </li>
+        {% endfor %}
+
+        {% if objects.has_next %}
+            <li class="page-item">
+                <a class="page-link" href="?page={{ objects.next_page_number }}" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        {% endif %}
+    </ul>
+</nav>
