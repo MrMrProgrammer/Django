d18ac150b37365b6d34359ea541e9a8780c68ec0
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-11-15 13:58:48 +0330

    Create Log app

--- Save Log.py
+++ Save Log.py
@@ -76,3 +76,34 @@ admin.site.register(models.Logger, showLogs)
 
 
 # ===========================================================
+# Log/models.py
+
+
+from django.db import models
+
+class Logger(models.Model):
+    date = models.CharField(max_length=20, verbose_name="تاریخ")
+    time = models.CharField(max_length=20, verbose_name="زمان")
+    ip = models.CharField(max_length=20, verbose_name="IP")
+    agent = models.CharField(max_length=500, verbose_name="مرورگر")
+    language = models.CharField(max_length=100, verbose_name="زبان")
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "بازدید"
+        verbose_name_plural = "بازدید ها"
+
+
+# ===========================================================
+# On the target page
+# views.py
+
+from Log.loggers import saveLog
+
+# --------------
+saveLog(request)
+# --------------
+
+# ===========================================================
