bfa6366cd2f5956f317e4b1f0ac8af2d75b5fb42
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-13 10:25:01 +0330

    add second tutorial link

--- Create Visual Editor.py
+++ Create Visual Editor.py
@@ -1,4 +1,9 @@
 # Tutorial Link
 
+# 1)
 # https://fengmei.medium.com/adding-a-wysiwyg-editor-to-your-django-forms-2d9a0e47e755
 # Youtube : https://www.youtube.com/watch?v=Rh7THG1-THU
+
+
+# 2)
+# https://www.geeksforgeeks.org/adding-wysiwyg-editor-to-django-project/
