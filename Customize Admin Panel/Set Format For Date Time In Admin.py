292003fa4011e426a41c891f8bc7a0d32cc9e7e8
MrMrProgrammer <mr.mr.programmer@gmail.com>
2024-01-17 17:18:34 +0330

    Set Format For Date Time In Admin

--- Customize Admin Panel/Set Format For Date Time In Admin.py
+++ Customize Admin Panel/Set Format For Date Time In Admin.py
@@ -1,10 +1,14 @@
-# 
+# ===========================================================
+# app/admin.py
 
 
-# class showContactUs(admin.ModelAdmin):
-#     list_display = ["name", "email", "subject", "message", "formatted_datetime", 'ip']
+class showContactUs(admin.ModelAdmin):
+    list_display = ["name", "email", "subject", "message", "formatted_datetime", 'ip']
 
-#     def formatted_datetime(self, obj):
-#         return obj.datetime.strftime('%H:%M | %Y-%m-%d')
+    def formatted_datetime(self, obj):
+        return obj.datetime.strftime('%H:%M | %Y-%m-%d')
 
-# admin.site.register(models.ContactUs, showContactUs)
+admin.site.register(models.ContactUs, showContactUs)
+
+
+# ===========================================================
